67d3b4432c9a2e2466442be8d98f2fdb09172c97
MatheusFranzener <felipe_mielke-vieira@estudante.sc.senai.br>
2022-05-11 21:33:35 -0300

    11/05

--- idleClicker/src/app/mundo1/mundo1.component.ts
+++ idleClicker/src/app/mundo1/mundo1.component.ts
@@ -9,8 +9,12 @@ export class Mundo1Component implements OnInit {
 
   constructor() { }
 
-  mps = 1;
+  mps = 0;
   moedas = 0;
+  click = 1;
+
+  custoClicar = 10;
+  custoMps = 100;
 
   ngOnInit() {
     this.moedasPorSegundo();
@@ -23,5 +27,25 @@ export class Mundo1Component implements OnInit {
       self.moedas += self.mps;
       self.moedasPorSegundo();
     }, 1000)
+  }
+
+  compra(numero) {
+    if(numero == 1) {
+      if(this.moedas >= this.custoMps) {
+        this.moedas -= this.custoMps;
+        this.mps += 1;
+        this.custoMps += 15;
+      }
+    } else {
+      if(this.moedas >= this.custoClicar) {
+        this.moedas -= this.custoClicar;
+        this.click += 1;
+        this.custoClicar += 9;
+      }
+    }
+  }
+
+  clicar() {
+    this.moedas += this.click
   }
 }
